3ccd18adfb60d1ef4d1f50fa6fa45bd87ee036a7
hernanluisalberto42 <hernanluisalberto42@gmail.com>
2023-02-13 23:14:20 -0500

    first commit react generator qr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
+import QrCode from 'qrcode'
+import ChromePicker from 'react-color'
 
 function App() {
+
+  const [text, setText] = useState("");
+  const [qrCode, setQrCode] = useState("");
+  const [error, seterror] = useState(false)
+  
+
+  const GenerateQr=()=>{
+    if(text.trim()===''){
+      seterror(true)
+      return;
+    }
+    seterror(false);
+    QrCode.toDataURL(text,(err,text)=>{
+           if(err) return console.log(err)
+
+           console.log(text)
+           setQrCode(text)
+    })
+    setText("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Generador de QR</h1>
+          <input
+            type="text"
+            placeholder='ejemplo http://www.google.com'
+            value={text}
+            onChange={(e)=>setText(e.target.value)}
+          />          
+          <button
+            onClick={()=>GenerateQr()}
+          >Generar</button>
+          {error?<p>Debes escribir algo</p>:null}
+           {qrCode && <>
+              <img src={qrCode} alt="text"/>
+              <a href={qrCode} download="qrCode.png">Descargar</a>
+           </>}
     </div>
   );
 }
